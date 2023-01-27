1172d1e771cfea8a373024fadcd8b7eb4aa6154d
MalcarAle <alexandre_malcar@icloud.com>
2023-01-26 21:26:59 -0300

    Projeto de DEVLINKS

---
+++ script.js
@@ -0,0 +1,22 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+  const alt = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    // substituir a imagem
+    img.setAttribute('src', 'assets/avatar-light.png')
+    alt.setAttribute("alt", "profile-image-light-mode")
+
+  } else {
+    img.setAttribute('src', 'assets/avatar.png')
+    alt.setAttribute("alt", "profile-image-dark-mode")
+
+  }
+
+  // se tiver light mode, adicionar imagem light
+  // se tiver normal, adicionar imagem normal
+}
